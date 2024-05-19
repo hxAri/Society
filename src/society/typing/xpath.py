125566bcf7b99dfbe524b08920b0b68743794f37
hxAri <hxari@proton.me>
2024-05-20 01:24:46 +0700

    [CREATE] Initial commit

---
+++ src/society/typing/xpath.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+#
+# @author Ari Setiawan
+# @create 12.01-2024 14:31
+# @github https://github.com/hxAri/Society
+#
+# Society Copyright (c) 2024 - Ari Setiawan <hxari@proton.me>
+# Society Licence under GNU General Public Licence v3
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+#
+# Society Program is not affiliated with or endorsed, endorsed at all by
+# Facebook or any other party, if you use the main account to use this
+# tool we as Coders and Developers are not responsible for anything,
+# use it at your own risk, and this is Strictly not for SPAM.
+#
+
+from builtins import str as Str
+from typing import final
+
+
+@final
+class Xpath:
+	
+	""" Facebook Xpath Typing Implementation """
+	
+	AnchorHome:Str = "/html/body/div[1]/div/div[1]/div/div[2]/div[4]/div/div[1]/div[1]/ul/li[1]/span/div/a"
+	ButtonLogin:Str = "/html/body/div[1]/div[1]/div[1]/div/div/div/div[2]/div/div[1]/form/div[2]/button"
+	FormLogin:Str = "/html/body/div[1]/div[1]/div[1]/div/div/div/div[2]/div/div[1]"
+	InputEmail:Str = "/html/body/div[1]/div[1]/div[1]/div/div/div/div[2]/div/div[1]/form/div[1]/div[1]/input"
+	inputpassword:Str = "/html/body/div[1]/div[1]/div[1]/div/div/div/div[2]/div/div[1]/form/div[1]/div[2]/div/input"
+	
+	...
